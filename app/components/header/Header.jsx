aa6f7797b380ddd1ff0d90e47e38353f351569bc
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-07-31 21:07:37 +0100

    test no d

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -6,7 +6,7 @@ import Logo from "./Logo";
 const Header = () => {
   return (
     <>
-      <header className="absolute pt-8 z-30 w-full ">
+      <header className="fixed pt-8 z-30 w-full ">
         <div className="w-4/5 flex justify-between items-center mx-auto">
           <Logo />
           <Navbar />
