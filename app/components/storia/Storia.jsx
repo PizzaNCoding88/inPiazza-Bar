049bd200288c5b12ca8ae89ddfd4e6ad0800397d
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-08-15 17:18:19 +0100

    la nostra storia 0.6

--- app/components/storia/Storia.jsx
+++ app/components/storia/Storia.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import "./Storia.css";
 import Image from "next/image";
 import apertura from "../../../public/assets/apertura.webp";
+import cocktail from "../../../public/assets/cocktail1.webp";
+import champagne from "../../../public/assets/champagne.webp";
+import logo from "../../../public/assets/champagne.webp";
 
 const Storia = () => {
   return (
     <>
       <main>
-        <section className="about-us mt-4">
-          <div className="container">
+        <section>
+          <div className="container text-black">
             <h1 className="text-5xl font-medium ">Chi Siamo</h1>
             <div className="about-us__img-container">
               <Image
@@ -24,6 +27,81 @@ const Storia = () => {
               Siamo due amici che, dopo anni di duro lavoro, sacrifici e sogni,
               abbiamo finalmente deciso di realizzare il nostro sogno aprendo un
               bar che offrisse esperienze uniche e indimenticabili.
+            </p>
+          </div>
+        </section>
+        <section className="bg-primary">
+          <div className="container text-white">
+            <div className="about-us__img-container">
+              <Image
+                src={cocktail}
+                alt="opening pic"
+                className="rounded-tr-[0.4rem] rounded-bl-[0.4rem] rounded-tl-3xl rounded-br-3xl w-full h-full"
+              />
+            </div>
+            <p className="text-xl">
+              I nostri cocktail sono studiati attentamente, utilizzando solo gli
+              ingredienti più freschi e di prima qualità. Ogni drink è una
+              creazione originale, ideata per stupire e soddisfare i gusti più
+              raffinati.
+              <br /> <br />
+              Dalle classiche ricette dei cocktail tradizionali alle nostre
+              creazioni inedite, le nostre bevande sono pensate per sorprendere
+              e deliziare ogni vostro palato.
+            </p>
+          </div>
+        </section>
+
+        <section className="bg-form">
+          <div className="container text-black">
+            <div className="about-us__img-container">
+              <Image
+                src={champagne}
+                alt="opening pic"
+                className="rounded-tr-[0.4rem] rounded-bl-[0.4rem] rounded-tl-3xl rounded-br-3xl w-full h-full"
+              />
+            </div>
+            <p className="text-xl">
+              Ma la nostra offerta non si limita solo ai cocktail. Siamo anche
+              appassionati di champagne e vini pregiati.
+              <br /> <br />
+              Abbiamo selezionato una lista di champagne rinomati e vini
+              provenienti da diverse regioni del mondo, per offrire una vasta
+              scelta di etichette che si abbinino alla perfezione ai nostri
+              piatti e alle vostre preferenze.
+            </p>
+          </div>
+        </section>
+
+        <section className="bg-primary">
+          <div className="container text-white">
+            <div className="about-us__img-container">
+              <Image
+                src={logo}
+                alt="opening pic"
+                className="rounded-tr-[0.4rem] rounded-bl-[0.4rem] rounded-tl-3xl rounded-br-3xl w-full h-full"
+              />
+            </div>
+            <p className="text-xl">
+              Il nostro locale è un&apos;oasi di relax e divertimento, un luogo
+              in cui potrete trascorrere piacevoli serate in compagnia di amici
+              o familiari. <br />
+              <br />
+              Abbiamo curato ogni dettaglio dell&apos;arredamento per creare
+              un&apos;atmosfera accogliente, sofisticata e contemporanea, in cui
+              potrete sentirvi a vostro agio.
+            </p>
+          </div>
+        </section>
+
+        <section className="bg-form">
+          <div className="container text-black">
+            <p className="text-xl">
+              Vi aspettiamo nel nostro bar, champagneria e cocktail lounge per
+              farvi vivere esperienze uniche e indimenticabili. I nostri drink,
+              la nostra selezione di champagne e la nostra ospitalità vi
+              conquisteranno fin dal primo istante. Siete i benvenuti nel nostro
+              mondo di sapori, colori e sensazioni.
             </p>
           </div>
         </section>
