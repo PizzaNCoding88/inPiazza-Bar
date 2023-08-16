33f60ccdf26f2e97d4a2acd90437ee5982ac256f
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-08-16 23:18:59 +0100

    second menu test

--- app/components/foodMenu/FoodMenu.jsx
+++ app/components/foodMenu/FoodMenu.jsx
@@ -4,13 +4,27 @@ import React from "react";
 import "./FoodMenu.css";
 import Image from "next/image";
 import Menu from "../../../public/assets/menu-vini-1.webp";
+import { useState } from "react";
 
 const FoodMenu = () => {
+  const [zoomed, setZoomed] = useState();
+
+  const setZoom = () => {
+    setZoomed(!zoomed);
+  };
   return (
     <>
       <section>
-        <picture className="w-[300px] mx-auto">
-          <Image src={Menu} alt="menu" width={300} />
+        {/* <picture className="w-[400px] mx-auto"> */}
+        <picture
+          className={zoomed ? "w-full" : "w-[400px] mx-auto"}
+          onClick={setZoom}
+        >
+          <Image
+            src={Menu}
+            alt="menu"
+            width={zoomed ? "w-full" : "w-[400px] mx-auto"}
+          />
         </picture>
       </section>
     </>
