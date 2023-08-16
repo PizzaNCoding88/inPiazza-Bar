d13d74474d8a11222b6ba10784c86106518db4ac
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-08-16 23:41:31 +0100

    test animation menu

--- app/components/foodMenu/FoodMenu.jsx
+++ app/components/foodMenu/FoodMenu.jsx
@@ -5,6 +5,12 @@ import "./FoodMenu.css";
 import Image from "next/image";
 import Menu from "../../../public/assets/menu-vini-1.webp";
 import { useState } from "react";
+import { motion } from "framer-motion";
+
+const zoomVariants = {
+  open: { width: "100%" },
+  closed: { width: "400px" },
+};
 
 const FoodMenu = () => {
   const [zoomed, setZoomed] = useState();
@@ -15,20 +21,64 @@ const FoodMenu = () => {
   return (
     <>
       <section>
-        {/* <picture className="w-[400px] mx-auto"> */}
-        <picture
-          className={zoomed ? "w-full" : "w-[400px] mx-auto"}
+        <motion.picture
+          animate={zoomed ? "open" : "closed"}
+          variants={zoomVariants}
+          //   className={
+          //     zoomed
+          //       ? "w-full transition-all duration-700 origin-center"
+          //       : "w-[400px] mx-auto transition-all duration-700 origin-center"
+          //   }
           onClick={setZoom}
+          className="mx-auto"
         >
           <Image
             src={Menu}
             alt="menu"
             width={zoomed ? "w-full" : "w-[400px] mx-auto"}
           />
-        </picture>
+        </motion.picture>
       </section>
     </>
   );
 };
 
 export default FoodMenu;
+
+// "use client";
+
+// import React from "react";
+// import "./FoodMenu.css";
+// import Image from "next/image";
+// import Menu from "../../../public/assets/menu-vini-1.webp";
+// import { useState } from "react";
+
+// const FoodMenu = () => {
+//   const [zoomed, setZoomed] = useState();
+
+//   const setZoom = () => {
+//     setZoomed(!zoomed);
+//   };
+//   return (
+//     <>
+//       <section>
+//         <picture
+//           className={
+//             zoomed
+//               ? "w-full transition-all duration-700 origin-center"
+//               : "w-[400px] mx-auto transition-all duration-700 origin-center"
+//           }
+//           onClick={setZoom}
+//         >
+//           <Image
+//             src={Menu}
+//             alt="menu"
+//             width={zoomed ? "w-full" : "w-[400px] mx-auto"}
+//           />
+//         </picture>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default FoodMenu;
