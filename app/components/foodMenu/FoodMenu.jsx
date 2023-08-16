f3852484d1a4cab516ad51115514c81df56603b7
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-08-16 23:50:17 +0100

    menu files added

--- app/components/foodMenu/FoodMenu.jsx
+++ app/components/foodMenu/FoodMenu.jsx
@@ -25,11 +25,6 @@ const FoodMenu = () => {
           initial="closed"
           animate={zoomed ? "open" : "closed"}
           variants={zoomVariants}
-          //   className={
-          //     zoomed
-          //       ? "w-full transition-all duration-700 origin-center"
-          //       : "w-[400px] mx-auto transition-all duration-700 origin-center"
-          //   }
           onClick={setZoom}
           className="mx-auto"
         >
@@ -45,41 +40,3 @@ const FoodMenu = () => {
 };
 
 export default FoodMenu;
-
-// "use client";
-
-// import React from "react";
-// import "./FoodMenu.css";
-// import Image from "next/image";
-// import Menu from "../../../public/assets/menu-vini-1.webp";
-// import { useState } from "react";
-
-// const FoodMenu = () => {
-//   const [zoomed, setZoomed] = useState();
-
-//   const setZoom = () => {
-//     setZoomed(!zoomed);
-//   };
-//   return (
-//     <>
-//       <section>
-//         <picture
-//           className={
-//             zoomed
-//               ? "w-full transition-all duration-700 origin-center"
-//               : "w-[400px] mx-auto transition-all duration-700 origin-center"
-//           }
-//           onClick={setZoom}
-//         >
-//           <Image
-//             src={Menu}
-//             alt="menu"
-//             width={zoomed ? "w-full" : "w-[400px] mx-auto"}
-//           />
-//         </picture>
-//       </section>
-//     </>
-//   );
-// };
-
-// export default FoodMenu;
