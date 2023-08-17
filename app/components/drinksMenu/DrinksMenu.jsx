672f0a6ff2c27346c6afe8227be5f5ca3722b68b
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-08-17 17:48:23 +0100

    drinks menu page test

--- app/components/drinksMenu/DrinksMenu.jsx
+++ app/components/drinksMenu/DrinksMenu.jsx
@@ -5,14 +5,19 @@ import Image from "next/image";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Cocktails from "../../../public/assets/menu-cocktails.webp";
+import LiquoriChampagne from "../../../public/assets/menu-liquori-champagne.webp";
+import LiquoriDessert from "../../../public/assets/menu-liquori-desserts.webp";
+import Liquori from "../../../public/assets/menu-liquori.webp";
+import Vini from "../../../public/assets/menu-vini-1.webp";
+import Menu from "../menu/Menu.jsx";
 
 const zoomVariants = {
   open: { width: "100%" },
   closed: { width: "400px" },
 };
 
-const DrinksMenu = (props) => {
-  const { drinks } = props;
+const DrinksMenu = () => {
+  //
   const [zoomed, setZoomed] = useState(false);
 
   const setZoom = () => {
@@ -21,6 +26,14 @@ const DrinksMenu = (props) => {
   return (
     <>
       <section>
+        <Menu zoomed={zoomed} setZoom={setZoom} src={Cocktails} />
+        <Menu zoomed={zoomed} setZoom={setZoom} src={LiquoriChampagne} />
+        <Menu zoomed={zoomed} setZoom={setZoom} src={LiquoriDessert} />
+        <Menu zoomed={zoomed} setZoom={setZoom} src={Liquori} />
+        <Menu zoomed={zoomed} setZoom={setZoom} src={Vini} />
+      </section>
+
+      {/* <section>
         {drinks.map((drinks) => {
           return (
             <>
@@ -29,38 +42,24 @@ const DrinksMenu = (props) => {
                 animate={zoomed ? "open" : "closed"}
                 variants={zoomVariants}
                 onClick={setZoom}
-                className="mx-auto py-4"
+                className="mx-auto py-4 w-full h-full relative"
               >
                 <Image
                   src={drinks.src}
                   alt="menu"
                   //   width={zoomed ? "w-full" : "w-[400px] mx-auto"}
-                  //   width={400}
-                  //   height={400}
+                  width={zoomed ? "w-full" : "w-[400px] mx-auto"}
+                  //   width={300}
+                  //   height={300}
+                  //   objectFit="contain"
+                  //   //   fill="true"
                   //   layout="fill"
-                  objectFit="cover"
-                  fill={true}
+                  className="absolute"
                 />
               </motion.picture>
             </>
           );
         })}
-      </section>
-
-      {/* <section>
-        <motion.picture
-          initial="closed"
-          animate={zoomed ? "open" : "closed"}
-          variants={zoomVariants}
-          onClick={setZoom}
-          className="mx-auto py-4"
-        >
-          <Image
-            src={Cocktails}
-            alt="menu"
-            width={zoomed ? "w-full" : "w-[400px] mx-auto"}
-          />
-        </motion.picture>
       </section> */}
     </>
   );
--- app/drinks/page.jsx
+++ app/drinks/page.jsx
@@ -2,19 +2,9 @@ import React from "react";
 import DrinksMenu from "../components/drinksMenu/DrinksMenu";
 
 const page = () => {
-  const drinks = [
-    {
-      id: 1,
-      src: "/../public/assets/menu-cocktails.webp",
-    },
-    {
-      id: 2,
-      src: "/../public/assets/menu-liquori-champagne.webp",
-    },
-  ];
   return (
     <>
-      <DrinksMenu drinks={drinks} />
+      <DrinksMenu />
     </>
   );
 };
