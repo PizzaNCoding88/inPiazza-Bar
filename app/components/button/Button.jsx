18c7a639a4623a1d841d8633f86ac9c6549ec942
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-08-22 08:54:35 +0100

    landing page completed

--- app/components/button/Button.jsx
+++ app/components/button/Button.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./Button.css";
 import Link from "next/link";
 
-const Button = ({ text, link, style }) => {
+const Button = ({ text, link, style, linkstyle }) => {
   return (
     <>
-      <Link href={link}>
+      <Link href={link} className={`${linkstyle}`}>
         <button className={`btn btn_${style}`}>{text}</button>
       </Link>
     </>
