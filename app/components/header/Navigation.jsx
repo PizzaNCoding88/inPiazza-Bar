29723478a6b2a2eda6ec3e214c2919aee858375b
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-08-23 08:59:06 +0100

    desktop - navigation

---
+++ app/components/header/Navigation.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+
+const Navigation = () => {
+  const listItems = [
+    {
+      id: 0,
+      name: "Menu",
+      href: "/menu",
+    },
+    {
+      id: 1,
+      name: "La nostra storia",
+      href: "/storia",
+    },
+    {
+      id: 2,
+      name: "Prenotazioni",
+      href: "/prenotazioni",
+    },
+  ];
+  return (
+    <>
+      <ul className="flex gap-10 text-xl font-antic text-form">
+        {listItems.map((li) => {
+          return (
+            <Link key={li.id} href={li.href}>
+              <li>{li.name}</li>
+            </Link>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Navigation;
