5b8e297870c148df6be3228053ad40d4ff8ccfa7
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-08-23 22:18:55 +0100

    desktop - footer v1

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -14,46 +14,52 @@ const Footer = () => {
   return (
     <>
       <footer className="footer">
-        <div className="footer_container"></div>
-        <div className="footer_content">
-          <div className="footer_content_social">
-            <Link href={facebook}>
-              <Image alt="facebook" src={facebookIco} className="h-14 w-14" />
-            </Link>
-            <Link href={instagram}>
-              <Image alt="instagram" src={instagramIco} className="h-14 w-14" />
-            </Link>
-          </div>
-          <div className="footer_content_directions-address">
-            <div className="flex justify-center">
-              <Image
-                alt="location"
-                src={locationIco}
-                className="h-6 w-6 mr-1"
-              />
-              <p className="text-tertiary text-center font-antic">
-                Via Posidonia, 409, Salerno, Italy, 84129
-              </p>
+        <div className="footer_container">
+          <div className="hidden md:inline-block md:w-[216px] md:bg-blue-500"></div>
+          <div className="footer_content">
+            <div className="footer_content_social">
+              <Link href={facebook}>
+                <Image alt="facebook" src={facebookIco} className="h-14 w-14" />
+              </Link>
+              <Link href={instagram}>
+                <Image
+                  alt="instagram"
+                  src={instagramIco}
+                  className="h-14 w-14"
+                />
+              </Link>
             </div>
-            <div className="flex justify-center">
-              <Image alt="location" src={phoneIco} className="h-6 w-6 mr-1" />
-              <p className="text-tertiary text-center font-antic">
-                +39 089 935 5030
-              </p>
+            <div className="footer_content_directions-address">
+              <div className="flex justify-center">
+                <Image
+                  alt="location"
+                  src={locationIco}
+                  className="h-6 w-6 mr-1"
+                />
+                <p className="text-tertiary text-center font-antic">
+                  Via Posidonia, 409, Salerno, Italy, 84129
+                </p>
+              </div>
+              <div className="flex justify-center">
+                <Image alt="location" src={phoneIco} className="h-6 w-6 mr-1" />
+                <p className="text-tertiary text-center font-antic">
+                  +39 089 935 5030
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="footer_content_policy-copyright">
-            <p>Copyright © 2023 - In Piazza</p>
-            <Link href="/privacy-policy">
-              <p className="border-b-[1px] border-b-solid border-b-tertiary pb-1 inline border-opacity-50">
-                Policy sulla Privacy
-              </p>
-            </Link>
-            <Link target="_blank" href="http://www.umbertonardiello.com">
-              <p className="border-b-[1px] border-b-solid border-b-tertiary pb-1 inline border-opacity-50">
-                Made by Umberto Nardiello
-              </p>
-            </Link>
+            <div className="footer_content_policy-copyright">
+              <p className="md:hidden">Copyright © 2023 - In Piazza</p>
+              <Link href="/privacy-policy">
+                <p className="border-b-[1px] border-b-solid border-b-tertiary pb-1 inline border-opacity-50">
+                  Policy sulla Privacy
+                </p>
+              </Link>
+              <Link target="_blank" href="http://www.umbertonardiello.com">
+                <p className="border-b-[1px] border-b-solid border-b-tertiary pb-1 inline border-opacity-50">
+                  Made by Umberto Nardiello
+                </p>
+              </Link>
+            </div>
           </div>
         </div>
       </footer>
