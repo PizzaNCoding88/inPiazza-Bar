67d1ec068622f711f7af12196d8d689b7a0fe4fa
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-08-25 08:31:31 +0100

    desktop - la nostra storia page completed

--- app/components/storia/Storia.jsx
+++ app/components/storia/Storia.jsx
@@ -18,7 +18,7 @@ const Storia = () => {
             <h1 className="text-5xl font-medium [text-shadow:_4px_4px_10px_rgba(0,0,0,0.8)] ">
               Chi Siamo
             </h1>
-            <div className="lg:flex flex-row lg:items-center gap-8">
+            <div className="lg:flex lg:flex-row lg:items-center gap-8">
               <div className="about-us__img-container">
                 <Image
                   src={apertura}
@@ -40,71 +40,77 @@ const Storia = () => {
         </section>
         <section className="bg-primary">
           <div className="storia_container text-white">
-            <div className="about-us__img-container">
-              <Image
-                src={cocktail}
-                alt="opening pic"
-                objectFit="cover"
-                fill="true"
-                className="rounded-tr-[0.4rem] rounded-bl-[0.4rem] rounded-tl-3xl rounded-br-3xl w-full h-full"
-              />
+            <div className="lg:flex lg:flex-row-reverse lg:items-center gap-8">
+              <div className="about-us__img-container">
+                <Image
+                  src={cocktail}
+                  alt="opening pic"
+                  objectFit="cover"
+                  fill="true"
+                  className="rounded-tr-[0.4rem] rounded-bl-[0.4rem] rounded-tl-3xl rounded-br-3xl w-full h-full"
+                />
+              </div>
+              <p className="text-xl">
+                I nostri cocktail sono studiati attentamente, utilizzando solo
+                gli ingredienti più freschi e di prima qualità. Ogni drink è una
+                creazione originale, ideata per stupire e soddisfare i gusti più
+                raffinati.
+                <br /> <br />
+                Dalle classiche ricette dei cocktail tradizionali alle nostre
+                creazioni inedite, le nostre bevande sono pensate per
+                sorprendere e deliziare ogni vostro palato.
+              </p>
             </div>
-            <p className="text-xl">
-              I nostri cocktail sono studiati attentamente, utilizzando solo gli
-              ingredienti più freschi e di prima qualità. Ogni drink è una
-              creazione originale, ideata per stupire e soddisfare i gusti più
-              raffinati.
-              <br /> <br />
-              Dalle classiche ricette dei cocktail tradizionali alle nostre
-              creazioni inedite, le nostre bevande sono pensate per sorprendere
-              e deliziare ogni vostro palato.
-            </p>
           </div>
         </section>
 
         <section className="bg-form">
           <div className="storia_container text-black">
-            <div className="about-us__img-container">
-              <Image
-                src={champagne}
-                alt="opening pic"
-                objectFit="cover"
-                fill="true"
-                className="rounded-tr-[0.4rem] rounded-bl-[0.4rem] rounded-tl-3xl rounded-br-3xl w-full h-full"
-              />
+            <div className="lg:flex lg:flex-row lg:items-center gap-8">
+              <div className="about-us__img-container">
+                <Image
+                  src={champagne}
+                  alt="opening pic"
+                  objectFit="cover"
+                  fill="true"
+                  className="rounded-tr-[0.4rem] rounded-bl-[0.4rem] rounded-tl-3xl rounded-br-3xl w-full h-full"
+                />
+              </div>
+              <p className="text-xl">
+                Ma la nostra offerta non si limita solo ai cocktail. Siamo anche
+                appassionati di champagne e vini pregiati.
+                <br /> <br />
+                Abbiamo selezionato una lista di champagne rinomati e vini
+                provenienti da diverse regioni del mondo, per offrire una vasta
+                scelta di etichette che si abbinino alla perfezione ai nostri
+                piatti e alle vostre preferenze.
+              </p>
             </div>
-            <p className="text-xl">
-              Ma la nostra offerta non si limita solo ai cocktail. Siamo anche
-              appassionati di champagne e vini pregiati.
-              <br /> <br />
-              Abbiamo selezionato una lista di champagne rinomati e vini
-              provenienti da diverse regioni del mondo, per offrire una vasta
-              scelta di etichette che si abbinino alla perfezione ai nostri
-              piatti e alle vostre preferenze.
-            </p>
           </div>
         </section>
 
         <section className="bg-primary">
           <div className="storia_container text-white">
-            <div className="about-us__img-container">
-              <Image
-                src={window}
-                alt="opening pic"
-                objectFit="cover"
-                fill="true"
-                className="rounded-tr-[0.4rem] rounded-bl-[0.4rem] rounded-tl-3xl rounded-br-3xl w-full h-full"
-              />
+            <div className="lg:flex lg:flex-row-reverse lg:items-center gap-8">
+              <div className="about-us__img-container">
+                <Image
+                  src={window}
+                  alt="opening pic"
+                  objectFit="cover"
+                  fill="true"
+                  className="rounded-tr-[0.4rem] rounded-bl-[0.4rem] rounded-tl-3xl rounded-br-3xl w-full h-full"
+                />
+              </div>
+              <p className="text-xl">
+                Il nostro locale è un&apos;oasi di relax e divertimento, un
+                luogo in cui potrete trascorrere piacevoli serate in compagnia
+                di amici o familiari. <br />
+                <br />
+                Abbiamo curato ogni dettaglio dell&apos;arredamento per creare
+                un&apos;atmosfera accogliente, sofisticata e contemporanea, in
+                cui potrete sentirvi a vostro agio.
+              </p>
             </div>
-            <p className="text-xl">
-              Il nostro locale è un&apos;oasi di relax e divertimento, un luogo
-              in cui potrete trascorrere piacevoli serate in compagnia di amici
-              o familiari. <br />
-              <br />
-              Abbiamo curato ogni dettaglio dell&apos;arredamento per creare
-              un&apos;atmosfera accogliente, sofisticata e contemporanea, in cui
-              potrete sentirvi a vostro agio.
-            </p>
           </div>
         </section>
 
