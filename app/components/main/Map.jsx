66339e67227e487882c4734028d13b833ebf655b
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-08-30 00:42:56 +0100

    contacts external sizes ok

--- app/components/main/Map.jsx
+++ app/components/main/Map.jsx
@@ -8,8 +8,8 @@ const Map = () => {
     "https://www.google.com/maps/place/in.Piazza+%7C+Aperichic+bar/@40.6639703,14.7904947,16.25z/data=!4m6!3m5!1s0x133bc3c33c8b280f:0x7099dfd151dacb6!8m2!3d40.6641779!4d14.7954408!16s%2Fg%2F11trlvt2qd?entry=ttu";
   return (
     <>
-      <Link href={mapsLink} target="_blank" className="">
-        <div className="w-full md:h-full md:aspect-square">
+      <Link href={mapsLink} target="_blank" className="lg:w-full">
+        <div className="w-full md:h-full md:aspect-square md:w-full">
           <Image
             alt="map"
             src={MapImg}
