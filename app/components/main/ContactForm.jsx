1adc47b8e40bc98aaaebada469c697d23151a2b5
PizzaNCoding88 <umbertonardiello@gmail.com>
2023-08-30 00:59:45 +0100

    contacts card completed

--- app/components/main/ContactForm.jsx
+++ app/components/main/ContactForm.jsx
@@ -5,59 +5,58 @@ const ContactForm = () => {
     <>
       <form
         //   onSubmit={onSubmit}
-        className="bg-form rounded-tr-lg rounded-tl-lg md:rounded-tr-none md:rounded-bl-lg aspect-square md:min-w-[50%]
-         "
+        className="bg-form rounded-tr-lg rounded-tl-lg md:rounded-tr-none md:rounded-bl-lg aspect-square md:min-w-[50%] pt-[5%] px-10"
       >
-        <h3 className="text-center text-primary font-bold  font-PrimaryF lg:font-SecondaryF ">
-          Inviaci un messaggio
-        </h3>
-        <label
-          htmlFor="name"
-          className="text-secondary font-PrimaryF "
-          aria-label="name"
-        >
+        <div className="flex flex-col justify-between h-full">
+          <h3 className="text-center text-primary font-bold  font-PrimaryF lg:font-SecondaryF text-xl sm:text-2xl md:text-xl lg:text-2xl xl:text-3xl ">
+            Inviaci un messaggio
+          </h3>
+          <label
+            htmlFor="name"
+            className="text-secondary font-PrimaryF "
+            aria-label="name"
+          >
+            <input
+              type="text"
+              id="name"
+              className="bg-transparent border-b border-b-secondary w-full pl-2 pb-2  text-primary focus:outline-none placeholder:text-primary placeholder:font-antic placeholder:font-bold sm:placeholder:text-xl md:placeholder:text-base lg:placeholder:text-xl xl:placeholder:text-2xl"
+              placeholder="Nome"
+              name="name"
+              required
+            ></input>
+          </label>
+          <label
+            htmlFor="email"
+            aria-label="email"
+            className="text-primary font-PrimaryF xl:text-xl"
+          >
+            <input
+              type="email"
+              id="email"
+              className="bg-transparent block border-b border-b-secondary w-full pb-2 pl-2  text-primary focus:outline-none placeholder:text-primary placeholder:font-antic placeholder:font-bold sm:placeholder:text-xl md:placeholder:text-base lg:placeholder:text-xl xl:placeholder:text-2xl"
+              placeholder="Email"
+              name="email"
+              required
+            ></input>
+          </label>
+          <label htmlFor="textarea" aria-label="textaread">
+            <textarea
+              id="textarea"
+              name="Text"
+              className="bg-transparent  border-b border-b-secondary w-full pl-2 pb-2 text-primary focus:outline-none placeholder:text-primary placeholder:font-antic placeholder:font-bold sm:placeholder:text-xl md:placeholder:text-base lg:placeholder:text-xl xl:placeholder:text-2xl"
+              placeholder="Message"
+            ></textarea>
+          </label>
           <input
-            type="text"
-            id="name"
-            className="bg-transparent border-b border-b-secondary w-full pl-2 pb-2  text-primary focus:outline-none placeholder:text-primary placeholder:font-antic placeholder:font-bold "
-            placeholder="Nome"
-            name="name"
-            required
-          ></input>
-        </label>
-
-        <label
-          htmlFor="email"
-          aria-label="email"
-          className="text-primary pl-2 font-PrimaryF xl:text-xl"
-        >
-          <input
-            type="email"
-            id="email"
-            className="bg-transparent block border-b border-b-secondary w-full pl-2 pb-2  text-primary focus:outline-none placeholder:text-primary placeholder:font-antic placeholder:font-bold"
-            placeholder="Email"
-            name="email"
-            required
-          ></input>
-        </label>
-
-        <label htmlFor="textarea" aria-label="textaread">
-          <textarea
-            id="textarea"
-            name="Text"
-            className="bg-transparent  border-b border-b-secondary w-full pl-2 pb-2 text-primary focus:outline-none placeholder:text-primary placeholder:font-antic placeholder:font-bold"
-            placeholder="Message"
-          ></textarea>
-        </label>
-        <input
-          type="submit"
-          className="text-primary bg-transparent border-2 border-secondary px-7 py-1 rounded-lg block mx-auto hover:bg-secondary hover:text-additional
-          transition-all duration-150 font-PrimaryF xl:mt-12"
-          aria-label="submit"
-        />
-        <span className=" text-primary mx-auto text-center block">
-          {/* RESULT HERE */}
-        </span>
+            type="submit"
+            className="text-primary bg-transparent border-2 border-secondary px-7 py-1 rounded-lg block mx-auto hover:bg-secondary hover:text-additional
+            transition-all duration-150 font-PrimaryF xl:mt-12 sm:text-xl md:text-base lg:text-xl xl:text-2xl"
+            aria-label="submit"
+          />
+          <span className=" text-primary mx-auto text-center block">
+            {/* RESULT HERE */}
+          </span>
+        </div>
       </form>
       {/* <form
         //   onSubmit={onSubmit}
