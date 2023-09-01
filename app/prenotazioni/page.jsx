e1e0570be76acd1dfd3bba57cb47ce535955d485
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-09-01 12:26:41 +0100

    prenotazioni css adjusted

--- app/prenotazioni/page.jsx
+++ app/prenotazioni/page.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Prenotazioni from "../components/prenotazioni/Prenotazioni";
+import "../components/prenotazioni/Prenotazioni.css";
+import "../components/main/Main.css";
 
 const page = () => {
   return <Prenotazioni />;
