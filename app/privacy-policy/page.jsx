3bcaf0636bbba92c626347562765c38e234b006a
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-09-06 15:50:38 +0100

    privacy policy animation

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -1,12 +1,20 @@
+"use client";
+
 import React from "react";
 import Policy from "../components/ppolicy/Policy";
+import { motion } from "framer-motion";
 
 const page = () => {
   return (
     <>
-      <main className="bg-customGrey text-black">
+      <motion.main
+        className="bg-customGrey text-black"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.8, ease: "easeOut" }}
+      >
         <Policy />
-      </main>
+      </motion.main>
     </>
   );
 };
