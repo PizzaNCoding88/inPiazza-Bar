b6125a207c26b47edb3b16a411b850c51b8b9e04
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-09-07 08:46:56 +0100

    main page images animated

--- app/components/main/Card.jsx
+++ app/components/main/Card.jsx
@@ -1,9 +1,12 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import radioCity from "../../../public/assets/radiocity_1.webp";
 import cocktails from "../../../public/assets/cocktail1.webp";
 import aperitivo from "../../../public/assets/aperitivo1.webp";
 import esterno from "../../../public/assets/esterno1.webp";
+import { motion } from "framer-motion";
 
 const Card = () => {
   const card = [
@@ -42,20 +45,30 @@ const Card = () => {
                 i % 2 === 0 ? "" : "odd"
               } ${i === 0 ? "" : "consecutives"}`}
             >
-              <div className="images-section_pic-container">
+              <motion.div
+                className="images-section_pic-container"
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 2, delay: 0.2 }}
+                viewport={{ once: true }}
+              >
                 <Image
                   alt={c.alt}
                   src={c.link}
                   style={{ objectFit: "cover" }}
                   fill={true}
                 />
-              </div>
-              <div
+              </motion.div>
+              <motion.div
                 className={`text-center ${
                   i % 2 === 0
                     ? "images-section_text-container-right"
                     : "images-section_text-container-left"
                 }`}
+                initial={{ x: `${i % 2 === 0 ? "30%" : "-30%"}`, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 1.5 }}
+                viewport={{ once: true }}
               >
                 <h2 className="mt-4 text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
                   {c.title}
@@ -63,7 +76,7 @@ const Card = () => {
                 <p className="mt-2 md:text-xl md:pt-2 lg:text-2xl xl:text-3xl">
                   {c.subtitle}
                 </p>
-              </div>
+              </motion.div>
             </div>
           </>
         );
