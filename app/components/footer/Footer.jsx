6f64e7506a43b0821df61b7b0539f578bd3521ef
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-09-14 17:19:09 +0100

    footer redesigned

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="footer_container_large-screens">
+        {/* <div className="footer_container_large-screens">
           <div className="footer_left-part-desktop">
             <div className="h-16 w-16">
               <Image src={Logo} alt="logo" className="rounded-full" />
@@ -122,6 +122,70 @@ const Footer = () => {
                 </p>
               </Link>
             </div>
+          </div>
+        </div> */}
+        <div className="hidden md:flex justify-around bg-slate-900 bg-opacity-60 gap-0">
+          <div className="px-4 py-6 lg:py-8">
+            <div className="flex justify-center gap-4 mb-4">
+              <Link href={instagram} target="_blank">
+                <Image
+                  alt="instagram"
+                  src={instagramIco}
+                  className="h-10 w-10 1440:h-10 1440:w-10"
+                />
+              </Link>
+              <Link href={facebook} target="_blank">
+                <Image
+                  alt="facebook"
+                  src={facebookIco}
+                  className="h-10 w-10 1440:h-10 1440:w-10"
+                />
+              </Link>
+            </div>
+            <div className="flex items-center gap-2 mb-4">
+              <Image
+                alt="location"
+                src={locationIco}
+                className="h-5 w-5 1440:h-6 1440:w-6 "
+              />
+              <p className="footer_address-phone">
+                Via Posidonia, 409, Salerno, Italy, 84129
+              </p>
+            </div>
+            <div className="flex justify-center gap-2">
+              <Image
+                alt="location"
+                src={phoneIco}
+                className="h-5 w-5 mr-1 1440:h-6 1440:w-6"
+              />
+              <Link href="tel:+39 089 935 5030">
+                <p className="footer_address-phone">+39 089 935 5030</p>
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center justify-center ">
+            <div className="px-12">
+              <Image
+                src={Logo}
+                alt="logo"
+                className="rounded-full h-16 w-16 lg:h-20 lg:w-20"
+              />
+            </div>
+          </div>
+          <div className="p-4 flex flex-col items-center justify-center gap-4">
+            <Link href="/privacy-policy">
+              <p className="font-antic text-sm lg:text-base 1440:text-xl">
+                Policy sulla Privacy
+              </p>
+            </Link>
+            <p className="text-sm lg:text-base 1440:text-xl">
+              Copyright © 2023 - In Piazza
+            </p>
+            <Link target="_blank" href="http://www.umbertonardiello.com">
+              <p className="inline text-sm lg:text-base 1440:text-xl font-antic">
+                Website by Umberto Nardiello
+              </p>
+            </Link>
           </div>
         </div>
       </footer>
