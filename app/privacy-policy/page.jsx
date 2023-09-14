f1dc974eeb598e027c544019e837d840319c7fd9
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-09-14 09:33:57 +0100

    policy privacy adjusted

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -1,8 +1,5 @@
-"use client";
-
 import React from "react";
 import Policy from "../components/ppolicy/Policy";
-import { motion } from "framer-motion";
 
 export const metadata = {
   title: "inPiazza Bar - Privacy Policy",
@@ -11,14 +8,7 @@ export const metadata = {
 const page = () => {
   return (
     <>
-      <motion.main
-        className="bg-customGrey text-black"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1.8, ease: "easeOut" }}
-      >
-        <Policy />
-      </motion.main>
+      <Policy />
     </>
   );
 };
